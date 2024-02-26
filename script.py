b36791e9ec004a913610abda593c9e4e42f5f606
18372738 <133884450+18372738@users.noreply.github.com>
2024-02-27 00:53:19 +0300

    Improved the code

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 import random
 
 from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation, Subject
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 
-HOW_TO_PRAISE = [
+
+
+COMMENDATIONS = [
     'Молодец!',
     'Отлично!',
     'Хорошо!',
@@ -38,14 +39,16 @@ HOW_TO_PRAISE = [
     ]
 
 
-
 def name_student(name):
     try:
         return Schoolkid.objects.get(full_name__contains=name)
     except Schoolkid.DoesNotExist:
         print(f"Ученик с именем '{name}'не найден.")
+        return
     except Schoolkid.MultipleObjectsReturned:
         print(f"Вы ничего не ввели или найдено несколько учеников с именем {name}, уточните ФИО.")
+        return
+
 
 def fix_marks(schoolkid):
     child = Mark.objects.filter(schoolkid=schoolkid, points__in=[2,3])
@@ -59,15 +62,17 @@ def remove_chastisements(schoolkid):
 
 def create_commendation(schoolkid, item_name):
     try:
-        get_subject = Subject.objects.get(title=item_name, year_of_study=schoolkid.year_of_study)
+        subject = Subject.objects.get(title=item_name, year_of_study=schoolkid.year_of_study)
     except Subject.DoesNotExist:
         print(f"Уточните название предмета {item_name}")
+        return
     except Subject.MultipleObjectsReturned:
         print(f"Предмета {item_name} для {schoolkid.year_of_study} класса не существует")
+        return
 
 
     last_lesson = Lesson.objects.filter(subject__title=item_name, group_letter=schoolkid.group_letter, year_of_study=schoolkid.year_of_study).order_by('-date').first()
     if last_lesson is None:
         print(f"{item_name} данного урока нет.")
     else:
-        praise = Commendation.objects.create(text=random.choice(HOW_TO_PRAISE), created=last_lesson.date, schoolkid=schoolkid, subject=last_lesson.subject, teacher=last_lesson.teacher)
+        praise = Commendation.objects.create(text=random.choice(COMMENDATIONS), created=last_lesson.date, schoolkid=schoolkid, subject=last_lesson.subject, teacher=last_lesson.teacher)
